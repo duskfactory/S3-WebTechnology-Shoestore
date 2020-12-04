a99d94329aa40430847a5d1d98e5d205b2d889f3
duskfactory <xander.storme@telenet.be>
2020-12-04 20:21:36 +0100

    add common blade template

---
+++ shoestore/resources/views/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title') - Elegance</title>
+    <meta charset="UTF-8" />
+    <meta name="author" content="Xander Storme" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+<header>
+    <img src="" alt="Elegance Logo" />
+</header>
+<nav>
+    <ul>
+        <li>Home</li>
+        <li>Profile</li>
+    </ul>
+</nav>
+<main>
+    @section('main')
+</main>
+<footer>
+    <p>&#169;2020 Elegance - Created by Xander Storme</p>
+</footer>
+</body>
+</html>
