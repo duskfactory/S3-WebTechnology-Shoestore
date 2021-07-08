ccdb778098e615e12754337304802a5ac64f63ce
Xander Storme <xander.storme@student.howest.be>
2021-07-08 19:53:40 +0200

    Update dashboard

--- shoestore/resources/views/dashboard.blade.php
+++ shoestore/resources/views/dashboard.blade.php
@@ -4,21 +4,42 @@
 @section('main')
 <div id="dashboard">
     <h1>Hello, @{{ user.name }}</h1>
-    <section>
-        <section>
+    <section id="overview">
+        <div>
+            <button onclick="openComments()">Comments</button>
+            <button onclick="openPurchases()">Purchases</button>
+        </div>
+        <div id="comments">
             <article v-for="comment in user.comments">
                 <h1>@{{ comment.title }}</h1>
                 <p>@{{ comment.body }}</p>
-                <img v-if="comment.image != null" :src="commentImage(comment.image)" />
+                <img v-if="comment.image != null" :src="image(comment.image)" />
             </article>
-        </section>
-        <section>
+        </div>
+        <div id="purchases">
             <article v-for="item in user.purchases">
-                <img :src="itemImage(item.image)" :alt="item.name" />
+                <img :src="image(item.image)" :alt="item.name" />
                 <p>@{{ item.name }}</p>
                 <p>@{{ item.price }}</p>
             </article>
-        </section>
+        </div>
     </section>
 </div>
+@endsection
+
+@section('scripts')
+<script>
+    const overview = {
+        data() {
+            return {
+                user: @json($user)
+            };
+        },
+        methods: {
+            image(url) {
+                return `https://shoestore.local/${url}`;
+            }
+        }
+    };
+</script>
 @endsection
